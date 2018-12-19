f2784d39fd93b517d7c75f7ed9710a63702c1a48
Neta Globerman <neta.globerman@gmail.com>
2018-12-19 16:24:23 +0200

    adding content to calculator specs

--- src/test/scala/io/omervk/CalculatorSpec.scala
+++ src/test/scala/io/omervk/CalculatorSpec.scala
@@ -1,9 +1,37 @@
 package io.omervk
 
-import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.{FreeSpec, Matchers}
 
-class CalculatorSpec extends FunSpec with Matchers {
-  it("should work") {
+class CalculatorSpec extends FreeSpec with Matchers {
+  "it should work when input is valid" in {
     Calculator.addAll(List(1, 2, 3)) should equal(6)
+  }
+
+  "it should throw an exception when input is null" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(null)
+  }
+
+  "it should throw an exception when input is an number" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(1)
+  }
+
+  "it should throw an exception when the list is empty" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(Nil)
+  }
+
+  "it should throw an exception when the list contains a null value" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(List(1, 2, null))
+  }
+
+  "it should throw an exception when the list contains a string" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(List("1"))
+  }
+
+  "it should throw an exception when the list contains a double" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(List(1.23))
+  }
+
+  "it should throw an exception when the list contains a negative number" in {
+    a[RuntimeException] should be thrownBy Calculator.addAll(List(-1))
   }
 }
